79c00585474348d6e544e4e75c6453cd9a691e61
張大為 <chou@alphabiz.co.jp>
2022-02-23 19:46:10 +0900

    add
    add

--- src/main/java/jp/co/alphabiz/controller/MemberController.java
+++ src/main/java/jp/co/alphabiz/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,13 +29,13 @@ public class MemberController extends AbstractController {
 	MemberService memberService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(Model model) {
+	public String index(@ModelAttribute("form") MemberForm form, Model model) {
 		logger.debug("member + index");
 		return "member/index";
 	}
 
 	@RequestMapping(value = "/search", method = RequestMethod.POST)
-	public String search(MemberForm form, Model model) {
+	public String search(@ModelAttribute("form") MemberForm form, Model model) {
 		logger.debug("Actor + search");
 		String name = form.getName();
 		String birthplaceId = form.getBirthplaceId();
